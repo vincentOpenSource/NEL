23a3669a7687d8455aeb60c63f684de1a43ab37e
NeoDun <1184833720@qq.com>
2018-01-09 09:52:34 +0800

    分页细节

--- dist/app.js
+++ dist/app.js
@@ -10544,17 +10544,17 @@ class Block {
             let ajax = new Ajax_1.Ajax();
             let blocks = yield ajax.post('getblocks', [pageUtil.pageSize, pageUtil.currentPage]);
             $("#blocks").empty();
-            if (pageUtil.totalPage - (pageUtil.currentPage + 1)) {
+            if (pageUtil.totalPage - pageUtil.currentPage) {
                 $("#next").removeClass('disabled');
             }
             else {
                 $("#next").addClass('disabled');
             }
             if (pageUtil.currentPage - 1) {
-                $("#next").removeClass('disabled');
+                $("#previous").removeClass('disabled');
             }
             else {
-                $("#next").addClass('disabled');
+                $("#previous").addClass('disabled');
             }
             blocks.forEach((item, index, input) => {
                 var newDate = new Date();
